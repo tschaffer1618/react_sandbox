c5f4eacdf4ee2ba6c71c6b7427b0d99f0a0e89bc
Tyler Schaffer <tpschaffer@gmail.com>
2020-03-01 23:08:00 -0700

    Explore JSX in comparison to HTML

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi, I'm a React App</h1>
+        <h1>Hi, this is a React App!</h1>
       </div>
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "Hi, this is a React app!"));
   }
 }
 
